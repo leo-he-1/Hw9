69ed4b467b432f1d90f20e53acbcb617a9a9ad1e
leo-he-1 <tuh28889@temple.edu>
2022-03-29 18:50:53 -0400

    added files

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function DashboardScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Dashboard</Text>
+    </View>
+  );
+}
+function LoginScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Login</Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+const[authToken, setAuthToken] = useState();
+
+const saveData = async () =>{
+  try {
+    await AsyncStorage.setItem('AuthToken','Lasha');
+  }catch(e){
+    console.log('Data did not save');
+  }
+}
+const getData = async () =>{
+  try {
+    const result = await AsyncStorage.getItem('AuthToken');
+    setAuthToken(result);
+  }catch (e){
+    console.log('fetching data failed');
+  }
+}
+saveData();
+//getData();
+
+
+
+  return (
+    <NavigationContainer>
+      <Button
+        title="Login to Dashboard"
+        onPress={() => getData()}
+      /> 
+      <Tab.Navigator>
+        {authToken === 'Lasha' ? (
+        <><Tab.Screen name="Dashboard" component={DashboardScreen} /><Tab.Screen name="Home" component={HomeScreen} /></>)
+        :
+        (<Tab.Screen name="Login" component={LoginScreen} />)}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
